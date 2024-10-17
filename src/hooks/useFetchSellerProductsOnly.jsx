40de4fec7e5e1f3d353a87cacff06d53ebe7a499
Khuzaima Kashif <83164491+khuzzzi@users.noreply.github.com>
2024-10-17 13:51:20 +0100

    Update useFetchSellerProductsOnly.jsx

--- src/hooks/useFetchSellerProductsOnly.jsx
+++ src/hooks/useFetchSellerProductsOnly.jsx
@@ -11,7 +11,7 @@ const useFetchSellerProductsOnly = () => {
   useEffect(() => {
     const fetchSellerOnlyProducts = async () => {
       try {
-        const response = await axios.get("http://localhost:3000/api/v1/seller/showSellerProducts", {
+        const response = await axios.get("https://new-ecommerce-platform-backend-1-7fqypb6de.vercel.app/api/v1/seller/showSellerProducts", {
           withCredentials: true
         });
 
